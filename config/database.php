4772cec0abb8e78b08e20718b25c4eb61af37ac1
Marc Mollá Lillo <marmollil@alu.edu.gva.es>
2025-05-30 17:05:37 +0200

    add SSL configuration for Redis connections in database.php

--- config/database.php
+++ config/database.php
@@ -141,13 +141,14 @@ return [
     |
     */
 
+
     'redis' => [
 
         'client' => env('REDIS_CLIENT', 'phpredis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
@@ -158,6 +159,9 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
+            'ssl' => [
+                'verify_peer' => false,  // O true si tienes certificados válidos
+            ],
         ],
 
         'cache' => [
@@ -167,8 +171,12 @@ return [
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
+            'ssl' => [
+                'verify_peer' => false,
+            ],
         ],
 
     ],
+
 
 ];
